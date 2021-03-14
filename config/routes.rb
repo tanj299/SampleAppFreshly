50ef215aafc40e84c9f8c0956c2cde2cbaabfd16
tanj299 <tanj299@gmail.com>
2021-03-14 17:02:33 -0400

    Clean-up foo controller and bar and baz URL, revise root to point to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
-  get 'static_pages/home'
+  root 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  root  'application#hello'
 end
